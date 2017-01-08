f72a4013c1132506d9b28fdbfe0c3c6c6eda00c1
bkd705 <brennenkdenomme@gmail.com>
2017-01-08 03:40:30 -0500

    Fixed error in environment file

--- config/environment.rb
+++ config/environment.rb
@@ -1,16 +1,17 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Initialize the Rails application.
+Rails.application.initialize!
+
 ActionMailer::Base.smtp_settings = {
   :port            => ENV['MAILGUN_SMTP_PORT'],
   :address         => ENV['MAILGUN_SMTP_SERVER'],
   :user_name       => ENV['MAILGUN_SMTP_LOGIN'],
   :password        => ENV['MAILGUN_SMTP_PASSWORD'],
   :domain          => 'aqueous-dawn-16232.herokuapp.com',
-  :authentication: => :plain,
+  :authentication => :plain,
 }
 
 ActionMailer::Base.delivery_method = :smtp
 
-# Initialize the Rails application.
-Rails.application.initialize!
