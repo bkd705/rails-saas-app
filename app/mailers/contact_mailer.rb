1101bcea7dbcb307258ab6e4c12bf1b2986544e5
bkd705 <brennenkdenomme@gmail.com>
2017-01-08 03:48:36 -0500

    Added comments to code

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,10 @@
 class ContactMailer < ActionMailer::Base
   default to: 'brennenkdenomme@gmail.com'
+  # Get instance variables from the contact form and send it as mail
   def contact_email(name, email, body)
     @name = name
     @email = email
     @body = body
-    mail(from: email, subject: 'Contact Form Message')
+    mail(from: name, subject: 'Contact Form Message')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
+  # resources has all rest routes, but we only want the 'create' POST route
   resources :contacts, only: :create
+  # change the get route, and rename it as 'new_contact'
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
